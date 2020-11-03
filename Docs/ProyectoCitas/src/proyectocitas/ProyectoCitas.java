1870b4cef5147dc53f05ca49d398179e99a552b6
maria <maria@DESKTOP-DNA7Q26>
2020-11-03 18:45:18 -0500

    Se sube la primera version del programa de citas

---
+++ Docs/ProyectoCitas/src/proyectocitas/ProyectoCitas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectocitas;
+
+/**
+ *
+ * @author maria
+ */
+public class ProyectoCitas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
